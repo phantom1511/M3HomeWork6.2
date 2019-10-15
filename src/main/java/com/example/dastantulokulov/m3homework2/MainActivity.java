2fb3b3ed33e448e1c020f695e2d76a0a4e7014c7
phantom1511 <dastan.tulokulov@gmail.com>
2019-10-15 13:47:51 +0600

    commit#1

--- src/main/java/com/example/dastantulokulov/m3homework2/MainActivity.java
+++ src/main/java/com/example/dastantulokulov/m3homework2/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
 
