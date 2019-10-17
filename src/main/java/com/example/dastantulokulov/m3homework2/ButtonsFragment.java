5019d2a09d989d7d854547eaab3439a48c852916
phantom1511 <dastan.tulokulov@gmail.com>
2019-10-17 14:21:20 +0600

    commit2.3

---
+++ src/main/java/com/example/dastantulokulov/m3homework2/ButtonsFragment.java
@@ -0,0 +1,47 @@
+package com.example.dastantulokulov.m3homework2;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ButtonsFragment extends Fragment {
+
+    Button btnHistory;
+    IButton listener;
+
+
+    public ButtonsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_buttons, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        btnHistory = getView().findViewById(R.id.historyBtn);
+
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.getHistory();
+            }
+        });
+    }
+}
